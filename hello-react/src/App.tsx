d0edb51e4227fecbdb2caa364fa20f537213558f
vineethayasa <vineethareddy185@gmail.com>
2023-08-01 21:06:24 +0530

    new commit

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -11,8 +11,8 @@ function App() {
       <div className="col-start-2 col-span-2 border border-black px-4 py-4 mt-6 rounded-lg mr-4 h-96">
         <div className="mt-2">
           <h1 className="text-2xl font-bold mb-4">Pending</h1>
-          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S" flag="p" />
-          <TaskCard title="Add a blog" dueDate="22nd March" assigneeName="Rohit M" flag="p" />
+          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S"/>
+          <TaskCard title="Add a blog" dueDate="22nd March" assigneeName="Rohit M"/>
           <div className="bg-gray-200 px-2 py-1 mt-4">
             <span className="flex items-center">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
@@ -26,8 +26,8 @@ function App() {
       <div className="col-start-4 ml-10 col-span-2 border border-black px-4 py-4 mt-6 rounded-lg h-96">
         <div className="mt-2">
           <h1 className="text-2xl font-bold mb-4">Done</h1>
-          <TaskCard title="Design the Mockup" completedAtDate="10th April" assigneeName="Rohit M" flag="d" />
-          <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S" flag="d" />
+          <TaskCard title="Design the Mockup" completedAtDate="10th April" assigneeName="Rohit M" />
+          <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S" />
         </div>
       </div>
     </div>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -6,20 +6,14 @@ interface TaskCard1 {
   dueDate?: string;
   completedAtDate?: string;
   assigneeName: string;
-  flag: 'p' | 'd';
 }
 
 const TaskCard = (props: TaskCard1) => {
-  const { flag } = props;
-
   return (
     <div className='TaskItem'>
       <h2 className="text-xl font-bold">{props.title}</h2>
-      {flag === 'p' ? (
-        <p>Due on: {props.dueDate}</p>
-      ) : flag === 'd' && (
-        <p>Completed on: {props.completedAtDate}</p>
-      )}
+      {props.dueDate && <p>Due on: {props.dueDate}</p>}
+      {props.completedAtDate && <p>Completed on: {props.completedAtDate}</p>}
       <p>Assignee: {props.assigneeName}</p>
     </div>
   );
