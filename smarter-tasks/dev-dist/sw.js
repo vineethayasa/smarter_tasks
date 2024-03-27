483aae3c7c0859d3303457e056b0b5510b2180f2
vineethayasa <vineethareddy185@gmail.com>
2024-03-27 23:00:22 +0530

    new commit

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4utq1hg58co"
+    "revision": "0.0d83cf7rdvo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
